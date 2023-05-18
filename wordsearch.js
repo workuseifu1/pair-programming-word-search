c3b56db7d8b31803080609ca478283be035746a1
workuseifu1 <workuseifu1@gmail.com>
2023-05-18 08:22:59 -0500

    complete

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,27 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+    if (letters.length === 0) {
+        return false;
     }
-}
+    const horizontalJoin = letters.map(ls => ls.join(''))
+    for (const l of horizontalJoin) {
+        if (l.includes(word)) {
+            return true;
+        }
+    }
+    const verticalJoin = [];
+    for (let i = 0; i < letters[0].length; i++) { 
+        let verticalStr = "";
+        for (let j = 0; j < letters.length; j++) { 
+            verticalStr += letters[j][i];
+        }
+        verticalJoin.push(verticalStr);
+    }
+    for (const l of verticalJoin) {
+        if (l.includes(word)) {
+            return true;
+        }
+    }
+    return false;
+};
 
 module.exports = wordSearch
